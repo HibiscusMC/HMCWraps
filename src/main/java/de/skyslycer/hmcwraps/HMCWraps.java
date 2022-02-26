f1fe605228b16456c07836704ae707cafa30287b
Skyslycer <skyslycer@gmx.net>
2022-02-26 21:17:17 +0100

    add prettier dependency checking

--- src/main/java/de/skyslycer/hmcwraps/HMCWraps.java
+++ src/main/java/de/skyslycer/hmcwraps/HMCWraps.java
@@ -1,7 +1,37 @@
 package de.skyslycer.hmcwraps;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HMCWraps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        if (!checkDependency("ProtocolLib", true)) {
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        if (checkDependency("PlaceholderAPI", false)) {
+            getLogger().info("Plugin 'PlaceholderAPI' found. Initializing hook.");
+        }
+    }
+
+    private boolean checkDependency(String name, boolean sendMessage) {
+        if (!Bukkit.getPluginManager().isPluginEnabled(name)) {
+            if (sendMessage) {
+                getLogger().severe(
+                        """
+                                =============================
+                                The plugin '""" + name + """
+                                ' is a required dependency but was not found on this server! Please restart the server after you have added the missing plugin!
+                                This plugin will shut down now.
+                                ============================="""
+                );
+            }
+            return false;
+        }
+        return true;
+    }
 
 }
---
+++ src/main/java/de/skyslycer/hmcwraps/util/StringUtil.java
@@ -0,0 +1,35 @@
+package de.skyslycer.hmcwraps.util;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver.Single;
+import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StringUtil {
+
+    public static final MiniMessage miniMessage = MiniMessage.builder().build();
+
+    public static BaseComponent[] parse(CommandSender sender, String message, Single... placeholders) {
+        String string = ChatColor.translateAlternateColorCodes('&', message);
+        Component component = miniMessage.deserialize(replacePlaceholders(sender, string), placeholders);
+        return BungeeComponentSerializer.get().serialize(component);
+    }
+
+    public static void send(CommandSender sender, String message, Single... placeholders) {
+        sender.spigot().sendMessage(parse(sender, message, placeholders));
+    }
+
+    private static String replacePlaceholders(CommandSender sender, String string) {
+        if (sender instanceof Player player && Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            return PlaceholderAPI.setPlaceholders(player, string);
+        }
+        return string;
+    }
+
+}
