4715402270f450e770f514ab1ac90eea9f65f087
Skyslycer <Skyslycer@users.noreply.github.com>
2023-09-28 21:05:10 +0200

    feat: add hit-any, hit-player and hit-entity to wrap actions
    Signed-off-by: Skyslycer <Skyslycer@users.noreply.github.com>

--- src/main/java/de/skyslycer/hmcwraps/listener/PlayerHitEntityListener.java
+++ src/main/java/de/skyslycer/hmcwraps/listener/PlayerHitEntityListener.java
@@ -1,6 +1,7 @@
 package de.skyslycer.hmcwraps.listener;
 
 import de.skyslycer.hmcwraps.HMCWrapsPlugin;
+import de.skyslycer.hmcwraps.actions.information.WrapActionInformation;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +22,22 @@ public class PlayerHitEntityListener implements Listener {
         }
         if (plugin.getPreviewManager().isPreviewing(player)) {
             plugin.getPreviewManager().remove(player.getUniqueId(), false);
+        }
+        var weapon = player.getInventory().getItemInMainHand();
+        if (weapon == null || weapon.getType().isAir()) {
+            return;
+        }
+        var wrap = plugin.getWrapper().getWrap(weapon);
+        if (wrap == null || wrap.getActions() == null) {
+            return;
+        }
+        if (wrap.getActions().get("hit-any") != null) {
+            plugin.getActionHandler().pushFromConfig(wrap.getActions().get("hit-any"), new WrapActionInformation(wrap, player, ""));
+        }
+        if (event.getEntity() instanceof Player && wrap.getActions().get("hit-player") != null) {
+            plugin.getActionHandler().pushFromConfig(wrap.getActions().get("hit-player"), new WrapActionInformation(wrap, player, ""));
+        } else if (wrap.getActions().get("hit-entity") != null) {
+            plugin.getActionHandler().pushFromConfig(wrap.getActions().get("hit-entity"), new WrapActionInformation(wrap, player, ""));
         }
     }
 
