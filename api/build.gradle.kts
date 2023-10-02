eb49ddff9dd8e1b13ed8aad8f84f745f11119a9b
Skyslycer <Skyslycer@users.noreply.github.com>
2023-10-02 18:43:19 +0200

    chore: update kotlin
    Signed-off-by: Skyslycer <Skyslycer@users.noreply.github.com>

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    kotlin("jvm") version "1.8.22"
-    id("org.jetbrains.dokka") version ("1.8.20")
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.dokka") version ("1.9.0")
 }
 
 group = "de.skyslycer.hmcwraps"
@@ -21,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.8.20")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.0")
     compileOnly(depends.spigot)
     compileOnly(depends.annotations)
     compileOnly(depends.placeholderapi)
