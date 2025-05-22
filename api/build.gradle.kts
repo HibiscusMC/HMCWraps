8f6c5f93c6035ee423e2abf46212b3853507a603
Skyslycer <noreply@skyslycer.de>
2025-05-22 20:47:05 +0200

    chore: bump versions
    Signed-off-by: Skyslycer <noreply@skyslycer.de>

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    kotlin("jvm") version "2.0.20"
-    id("org.jetbrains.dokka") version ("1.9.20")
+    kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.dokka") version ("2.0.0")
 }
 
 group = "de.skyslycer.hmcwraps"
@@ -26,7 +26,7 @@ repositories {
 }
 
 dependencies {
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:2.0.0")
     compileOnly(depends.spigot)
     compileOnly(depends.annotations)
     compileOnly(depends.placeholderapi)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,18 +9,18 @@ dependencyResolutionManagement {
             library("nexo", "com.nexomc:nexo:0.1.0-dev.14")
             library("oraxen", "com.github.oraxen:oraxen:-SNAPSHOT")
             library("itemsadder", "com.github.LoneDev6:API-ItemsAdder:3.6.1")
-            library("mythicmobs", "io.lumine:Mythic-Dist:5.7.1")
+            library("mythicmobs", "io.lumine:Mythic-Dist:5.8.0")
             library("annotations", "org.jetbrains:annotations:26.0.1")
         }
         create("libs") {
-            library("packets", "com.github.retrooper:packetevents-spigot:2.7.0")
+            library("packets", "com.github.retrooper:packetevents-spigot:2.8.0")
             library("particles", "com.owen1212055:particlehelper:1.5.0-SNAPSHOT")
             library("configupdater", "com.github.BG-Software-LLC:CommentedConfiguration:-SNAPSHOT")
             library("bstats", "org.bstats:bstats-bukkit:3.0.2")
-            library("gui", "dev.triumphteam:triumph-gui:3.1.11-SNAPSHOT")
+            library("gui", "dev.triumphteam:triumph-gui:3.2.0-SNAPSHOT")
             library("configurate", "org.spongepowered:configurate-yaml:4.1.2")
             library("mclogs", "gs.mclo:java:2.2.1")
-            library("nbtapi", "de.tr7zw:item-nbt-api:2.14.1")
+            library("nbtapi", "de.tr7zw:item-nbt-api:2.15.0")
             library("folialib", "com.tcoded:FoliaLib:0.4.2")
 
             library("adventure-api", "net.kyori", "adventure-api").versionRef("adventure")
